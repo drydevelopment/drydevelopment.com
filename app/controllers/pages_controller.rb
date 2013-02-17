4e02f9121907b340af3b8b2b8be0649e30196e6e
Doug Yoder <doug@drydevelopment.com>
2013-02-17 13:52:43 -0500

    added /wedding page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,9 @@ class PagesController < ApplicationController
   def about; end
   def contact; end
 
+
+  # Wedding landing page for RSVP and general info
+  #   - TODO: Take down after the wedding
+  def wedding; end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ DrydevelopmentCom::Application.routes.draw do
   match '/about' => 'pages#about', :as => 'about'
   match '/contact' => 'pages#contact', :as => 'contact'
 
+  # TODO: Remove after the wedding
+  match '/wedding' => 'pages#wedding', :as => 'wedding'
+
   root :to => 'pages#home'
 end
