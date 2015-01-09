e0efd05e292ca7bbf5e9df9d07808da1b0e7245f
Doug Yoder <doug@drydevelopment.com>
2015-01-08 22:20:33 -0500

    create gh-pages version

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DrydevelopmentCom::Application.config.secret_token = 'bd2e81e52844770d54be50187aea69fdfafc6e5b539ee02b7e61a269b908100ba9740dc60dcf9127c7cd6b3f0bf06ba577991ccbf9aa0ad9d16fb31ce73abbc4'
--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-DrydevelopmentCom::Application.routes.draw do
-  match '/about' => 'pages#about', :as => 'about'
-  match '/contact' => 'pages#contact', :as => 'contact'
-
-  # TODO: Remove after the wedding
-  match '/wedding' => 'pages#wedding', :as => 'wedding'
-
-  root :to => 'pages#home'
-end
